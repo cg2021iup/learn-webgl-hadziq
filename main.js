bbb2c750c4bbdeb0d2342fcfffa4137dae76799a
Hadziq Fabroyir <hadziq@gmail.com>
2021-09-10 09:09:28 +0700

    Fill the background with yellow

---
+++ main.js
@@ -0,0 +1,50 @@
+function main() {
+    // Access to the canvas through DOM: Document Object Model
+    var canvas1 = document.getElementById('canvas1');   // Prepare the paper
+    var gl = canvas1.getContext("webgl"); // Prepare the drawing tools: the brush, the paint, the pallete
+    // .getContext("webgl") will give us access to WebGL API
+
+    // There are two types of shader:
+    //  1. Vertex shader: responsible for manipulating vertex position
+    //  2. Fragment shader: responsible for manipulating the looks of the vertex (and the fragments between)
+    var vsSource = `
+        void main() {
+
+        }
+    `;
+    var fsSource = `
+        void main() {
+
+        }
+    `
+
+    // Create .c in GPU
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);   // Result in an empty vertexShader.c file
+    gl.shaderSource(vertexShader, vsSource);    // Fill the vertexShader.c file with the strings of code
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);   // Result in an empty fragmentShader.c file
+    gl.shaderSource(fragmentShader, fsSource);  // Fill the fragmentShader.c file with the strings of code
+
+    // Compile .c into .o
+    gl.compileShader(vertexShader); // Result in vertexShader.o
+    gl.compileShader(fragmentShader);   // Result in fragmentShader.o
+
+    // Prepare "the bowl" for the program: .exe
+    var shaderProgram = gl.createProgram();
+
+    // Put "the ingredients", vertexShader and fragmentShader, into "the bowl"
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+
+    // Let's mix it: Link the two .o files
+    gl.linkProgram(shaderProgram);  // The drawing tools are ready,
+                                    //  but our computer has not yet
+                                    //  held the drawing tools in its hands.
+    
+    // Let the computer hold the drawing tools
+    gl.useProgram(shaderProgram);
+
+    // Let the computer pick a color from the color pallete to fill the background
+    gl.clearColor(1.0, 1.0, 0.0, 1.0);
+    // Ask the computer to fill the background with the above color
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
