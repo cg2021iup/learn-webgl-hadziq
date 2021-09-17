ec45a1bdc99cb2f438df72b11ba589bc96043c93
Hadziq Fabroyir <hadziq@gmail.com>
2021-09-17 08:43:58 +0700

    Draw three points in different locations

--- main.js
+++ main.js
@@ -4,16 +4,36 @@ function main() {
     var gl = canvas1.getContext("webgl"); // Prepare the drawing tools: the brush, the paint, the pallete
     // .getContext("webgl") will give us access to WebGL API
 
+    // Define vertices data
+    /**
+     * A ( -0.5, -0.5 )
+     * B (  0.5, -0.5 )
+     * C (  0.5,  0.5 )
+     */
+
+    var vertices = [
+        -0.5, -0.5,         // A
+         0.5, -0.5,         // B
+         0.5,  0.5          // C
+    ];
+
+    // Create a linked-list for storing the vertices data
+    var buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
     // There are two types of shader:
     //  1. Vertex shader: responsible for manipulating vertex position
     //  2. Fragment shader: responsible for manipulating the looks of the vertex (and the fragments between)
     var vsSource = `
+        attribute vec2 aPosition;
         void main() {
             gl_PointSize = 10.0;
-            gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+            gl_Position = vec4(aPosition, 0.0, 1.0);
         }
     `;
     var fsSource = `
+        precision mediump float;
         void main() {
             gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
         }
@@ -44,10 +64,24 @@ function main() {
     // Let the computer hold the drawing tools
     gl.useProgram(shaderProgram);
 
+    // Teach the computer how to collect
+    //  the positional values from ARRAY_BUFFER
+    //  to each vertex being processed
+    var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
+    gl.vertexAttribPointer(
+        aPosition,
+        2,
+        gl.FLOAT,
+        false,
+        0,
+        0
+    );
+    gl.enableVertexAttribArray(aPosition);
+
     // Let the computer pick a color from the color pallete to fill the background
     gl.clearColor(1.0, 1.0, 0.0, 1.0);
     // Ask the computer to fill the background with the above color
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    gl.drawArrays(gl.POINTS, 0, 1);
+    gl.drawArrays(gl.POINTS, 0, 3);
 }
