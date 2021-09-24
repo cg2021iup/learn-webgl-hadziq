e89daed81d06d771ca60143e17f97f32e22ccdc1
Hadziq Fabroyir <hadziq@gmail.com>
2021-09-24 08:03:05 +0700

    Animate rectangle using setInterval

--- main.js
+++ main.js
@@ -33,9 +33,10 @@ function main() {
         attribute vec2 aPosition;
         attribute vec3 aColor;
         varying vec3 vColor;
+        uniform float uDelta;
         void main() {
             gl_PointSize = 10.0;
-            gl_Position = vec4(aPosition, 0.0, 1.0);
+            gl_Position = vec4(aPosition.x + uDelta, aPosition.y, 0.0, 1.0);
             vColor = aColor;
         }
     `;
@@ -96,10 +97,22 @@ function main() {
     );
     gl.enableVertexAttribArray(aColor);
 
-    // Let the computer pick a color from the color pallete to fill the background
-    gl.clearColor(1.0, 1.0, 0.0, 1.0);
-    // Ask the computer to fill the background with the above color
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    // Create a pointer to the Uniform variable we have on the shader
+    var uDelta = gl.getUniformLocation(shaderProgram, "uDelta");
+    var delta = 0;
 
-    gl.drawArrays(gl.TRIANGLES, 0, 6);
+    function render() {
+        // Build a linear animation
+        delta += 0.001;
+        console.log(delta);
+        gl.uniform1f(uDelta, delta);
+
+        // Let the computer pick a color from the color pallete to fill the background
+        gl.clearColor(1.0, 1.0, 0.0, 1.0);
+        // Ask the computer to fill the background with the above color
+        gl.clear(gl.COLOR_BUFFER_BIT);
+    
+        gl.drawArrays(gl.TRIANGLES, 0, 6);
+    }
+    setInterval(render, 1000/60);   // Frame rate: 60 frames per second
 }
